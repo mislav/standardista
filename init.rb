dae5de8ec43ce65d4dad96b0a4902a312651924e
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-10-20 00:39:36 +0200

    override ActionView::Helpers::InstanceTag#tag method, too

--- init.rb
+++ init.rb
@@ -1,5 +1,3 @@
 Haml::init_rails(binding) unless defined? Haml::Template
 require 'standardista'
 require 'standardista/implicit_tags'
-
-ActionView::Base.send :include, Standardista::Helpers
--- lib/standardista.rb
+++ lib/standardista.rb
@@ -29,11 +29,19 @@ module Standardista
     Haml::Template::options[:escape_html] = true
   end
   
-  module Helpers
+  module TagHelper
     # override tag helper from Rails to disable self-closing tags
-    # (there is no such thing in HTML)
     def tag(name, options = nil, open = false, escape = true)
       "<#{name}#{tag_options(options, escape) if options}>"
     end
+  end
+end
+
+ActionView::Base.send :include, Standardista::TagHelper
+
+# unfortunately, this duplication seems necessary :(
+ActionView::Helpers::InstanceTag.class_eval do
+  def tag_without_error_wrapping(name, options = nil, open = false, escape = true)
+    "<#{name}#{tag_options(options, escape) if options}>"
   end
 end
