718cb7614b61b928dea63aebad4e9f11527dcc42
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-05-21 17:35:20 +0200

    extract overrides for Rails tag() helper
    include them only if user hasn't opted for XHTML in the meantime

--- init.rb
+++ init.rb
@@ -1,3 +1,9 @@
 Haml::init_rails(binding) unless defined? Haml::Template
 require 'standardista'
 require 'standardista/implicit_tags'
+
+config.after_initialize do
+  unless :xhtml == Haml::Template::options[:format]
+    require 'standardista/rails_override'
+  end
+end
--- lib/standardista.rb
+++ lib/standardista.rb
@@ -28,20 +28,4 @@ module Standardista
   def self.auto_escape_html
     Haml::Template::options[:escape_html] = true
   end
-  
-  module TagHelper
-    # override tag helper from Rails to disable self-closing tags
-    def tag(name, options = nil, open = false, escape = true)
-      "<#{name}#{tag_options(options, escape) if options}>"
-    end
-  end
-end
-
-ActionView::Base.send :include, Standardista::TagHelper
-
-# unfortunately, this duplication seems necessary :(
-ActionView::Helpers::InstanceTag.class_eval do
-  def tag_without_error_wrapping(name, options = nil, open = false, escape = true)
-    "<#{name}#{tag_options(options, escape) if options}>"
-  end
 end
