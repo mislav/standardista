733cd2be5d86c0a69d703049a60028dc09a8b040
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-09-09 17:36:52 +0200

    don't try to bootstrap if Haml constant is not defined
    this handles the "rake gems:install" case where the
    application and plugins load, but not gems

--- init.rb
+++ init.rb
@@ -1,9 +1,12 @@
-Haml::init_rails(binding) unless defined? Haml::Template
-require 'standardista'
-require 'standardista/implicit_tags'
+if defined? Haml
+  Haml::init_rails(binding) unless defined? Haml::Template
 
-config.after_initialize do
-  unless :xhtml == Haml::Template::options[:format]
-    require 'standardista/rails_override'
+  require 'standardista'
+  require 'standardista/implicit_tags'
+
+  config.after_initialize do
+    unless :xhtml == Haml::Template::options[:format]
+      require 'standardista/rails_override'
+    end
   end
 end
